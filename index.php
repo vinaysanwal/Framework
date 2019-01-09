5cf28eeb589c8eba9546e5f1d18001e7d1c082b7
root <sanwalvinay@gmail.com>
2019-01-10 00:39:49 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>My Framework</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/index.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <nav class="nav-L5onzh">
+    <div class="pc-wrap relative">
+      <div id="exTab1" class="container">
+      <ul  class="nav nav-pills inner-3980SK">
+            <li class="cate-1xuqR9 active-2M2GTP">
+              <a  href="#1a" data-toggle="tab">Overview</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#2a" data-toggle="tab">Using nav-pills</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#3a" data-toggle="tab">Applying clearfix</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#4a" data-toggle="tab">Background color</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#4a" data-toggle="tab">Background color</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#4a" data-toggle="tab">Background color</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#4a" data-toggle="tab">Background color</a>
+            </li>
+            <li class="cate-1xuqR9"><a href="#4a" data-toggle="tab">Background color</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+    </br></br></br></br>
+
+            <div class="tab-content clearfix">
+              <div class="tab-pane active" id="1a">
+                <h3>Content's background color is the same for the tab</h3>
+              </div>
+              <div class="tab-pane" id="2a">
+                <h3>We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
+              </div>
+              <div class="tab-pane" id="3a">
+                <h3>We applied clearfix to the tab-content to rid of the gap between the tab and the content</h3>
+              </div>
+                <div class="tab-pane" id="4a">
+                <h3>We use css to change the background color of the content to be equal to the tab</h3>
+              </div>
+            </div>
+        </div>
+</body>
+</html>
